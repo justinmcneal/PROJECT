064c1744344b80ef442c32d3ef1ce8619bd20325
RipTin <carononganjustinmcneal24@gmail.com>
2024-04-23 08:34:22 +0800

    hhh

--- script.js
+++ script.js
@@ -9,31 +9,31 @@ function toggleMenu() {
 
 
   /* contact js */
-function sendMail() {
-  var parameter = {
-    firstname: document.getElementById("firstname").value,
-    lastname: document.getElementById("lastname").value,
-    email: document.getElementById("email").value,
-    phone: document.getElementById("phone").value,
-    message: document.getElementById("message").value,
-  };
-  
-  const serviceID = "service_1jvqwbr";
-  const templateID = "template_jp0zm0f";
-  
-  emailjs.send(serviceID, templateID, parameter)
-  .then(res=>{
-    document.getElementById("firstname").value = "";
-    document.getElementById("lastname").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
-    console.log(res);
-    alert("Manifesting Pasado!")
-  })
-  
-  .catch(err=>console.log(err));
-}
+  function sendMail() {
+    var parameter = {
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      message: document.getElementById("message").value,
+    };
+    
+    const serviceID = "service_1jvqwbr";
+    const templateID = "template_jp0zm0f";
+    
+    emailjs.send(serviceID, templateID, parameter)
+    .then(res=>{
+      document.getElementById("firstname").value = "";
+      document.getElementById("lastname").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Manifesting Pasado!")
+    })
+    
+    .catch(err=>console.log(err));
+  }
 
 /* discover js */
 
@@ -46,8 +46,6 @@ const thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-
-const timeAutoNext = 20000;
 
 let isAnimating = false; // Flag to track if animation is running
 
@@ -64,11 +62,6 @@ prevDom.addEventListener('click', () => {
         showSlider('prev');
     }
 });
-
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext);
 
 function showSlider(type) {
     const SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
@@ -89,6 +82,5 @@ function showSlider(type) {
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
         isAnimating = false; // Reset flag to indicate animation end
-    })
-
+    });
 }
